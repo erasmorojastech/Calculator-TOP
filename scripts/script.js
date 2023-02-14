fdd2c6c1a2a8dc05561ef21fc2cb15017e624a5d
erasmorojastech <erasmorojastech@gmail.com>
2023-02-14 17:58:38 -0300

    Add the logic that displays a number when is pressed

--- scripts/script.js
+++ scripts/script.js
@@ -30,3 +30,22 @@ const operate = function (operator, a, b) {
     return divide(a,b);
   }
 }
+
+// Display numbers on the screen
+const $number = document.querySelectorAll('.number');
+let $screen = document.querySelector('#screen');
+
+const populateScreen = function (number) {
+  
+  if ($screen.innerHTML === '0') {
+    $screen.innerHTML = number;
+  } else {
+    $screen.innerHTML = $screen.innerHTML + number;
+  }
+}
+
+$number.forEach((number)=>{
+  number.addEventListener('click', ()=>{
+    populateScreen(number.innerHTML);
+  })
+});
