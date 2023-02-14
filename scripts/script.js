282de5b00e6f9fb9b5bc6848fae12a88bf2e2f0a
erasmorojastech <erasmorojastech@gmail.com>
2023-02-14 16:06:17 -0300

    Create a new "operate" function
    This new function takes an operator and 2 numbers and then calls one
    of the arithmetic operation functions on the numbers.

--- scripts/script.js
+++ scripts/script.js
@@ -16,3 +16,17 @@ const multiply = (a, b) => {
 const divide = (a, b) => {
   return a / b;
 }
+
+// This function takes an operator and two numbers ank make an arithmetic operation
+const operate = function (operator, a, b) {
+  let result;
+
+  if (operator === '+') return add(a,b)
+  if (operator === '-') return subtract(a,b)
+  if (operator === '*') return multiply(a,b)
+  if (operator === '/' && b === 0) {
+    return alert(`It's not possible a division by zero`);
+  } else {
+    return divide(a,b);
+  }
+}
